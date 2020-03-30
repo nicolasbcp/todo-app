c7297946fde5ce253a3cdc0141f7ca3b55dff491
Nicolas Plaisant <nicolasbcp@gmail.com>
2020-03-30 15:05:14 -0300

    Handlers, Repositories e Casos de Testes

---
+++ Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Commands;
+
+namespace Todo.Domain.Tests.CommandTests
+{
+    [TestClass]
+    public class CreateTodoCommandTests
+    {
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Título da Tarefa", "nicolasplaisant", DateTime.Now);
+        
+        // Red, Green and Refactor
+        [TestMethod]
+        public void Dado_um_comando_invalido()
+        {
+            _invalidCommand.Validate();
+            Assert.AreEqual(_invalidCommand.Valid, false);
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido()
+        {
+            _validCommand.Validate();
+            Assert.AreEqual(_invalidCommand.Valid, true);
+        }
+    }
+}
---
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,48 @@
+using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Entities;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Handlers
+{
+    public class TodoHandler :
+        Notifiable,
+        IHandler<CreateTodoCommand>,
+        IHandler<UpdateTodoCommand>
+    {
+
+        private readonly ITodoRepository _repository;
+
+        public TodoHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(CreateTodoCommand command)
+        {
+            // Fail Fast Validation
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(
+                    false, 
+                    "Ops, parece que sua tarefa está errada!",
+                    command.Notifications);
+            
+            // Gera o TodoItem
+            var todo = new TodoItem(command.Title, command.Date, command.User);
+
+            // Salva no banco
+            _repository.Create(todo);
+
+            // Retorna o resultado
+            return new GenericCommandResult(true, "Tarefa salva com sucesso!", todo);
+        }
+
+        public ICommandResult Handle(UpdateTodoCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
