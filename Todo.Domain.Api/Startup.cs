f4b13fb2fcca632365fe2081c80003ecab73e47c
Nicolas Plaisant <nicolasbcp@gmail.com>
2020-04-01 18:30:48 -0300

    DB - SQL Server
    Migrations (EF Core 3) e tabelas geradas no SQL Server

--- Todo.Domain.Api/Startup.cs
+++ Todo.Domain.Api/Startup.cs
@@ -24,7 +24,11 @@ namespace Todo.Domain.Api
         {
             services.AddControllers();
             
-            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+            // Inicialmente foi usado DB em memória a fim de evitar
+            // possíveis novas migrations
+            // services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+
+            services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
 
             services.AddTransient<ITodoRepository, TodoRepository>();
             services.AddTransient<TodoHandler, TodoHandler>();
